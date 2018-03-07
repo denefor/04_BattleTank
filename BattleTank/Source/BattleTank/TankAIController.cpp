a22b2a13e634dd0826be79b0de7d79d2377db8e7
DESKTOP-V2HPTJL\Vicente <denefor@gmail.com>
2018-03-08 00:18:24 +0100

    BT57

--- BattleTank/Source/BattleTank/TankAIController.cpp
+++ BattleTank/Source/BattleTank/TankAIController.cpp
@@ -4,46 +4,20 @@
 #include "Tank.h"
 #include "Engine/World.h"
 
-void ATankAIController::BeginPlay()
-{
-	Super::BeginPlay();
-	ATank* PlayerTank = GetPlayerTank();
-	if (!PlayerTank)
-	{
-		UE_LOG(LogTemp, Warning, TEXT("AICOntroller can't find player tank"));
-	}
-	else
-	{
-		UE_LOG(LogTemp, Warning, TEXT("AICOntroller found player: %s"), *(PlayerTank->GetName()));
-	}
-}
-
 void ATankAIController::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-	ATank* PlayerTank = GetPlayerTank();
+
+	ATank* PlayerTank = Cast<ATank>(GetWorld()->GetFirstPlayerController()->GetPawn());
+	ATank* ControlledTank = Cast<ATank>(GetPawn());
+
 	if (PlayerTank)
 	{
 		// TODO Move towards the player
 
 		// Aim towards the player
-		GetControlledTank()->AimAt(PlayerTank->GetActorLocation());
+		ControlledTank->AimAt(PlayerTank->GetActorLocation());
 
-		// Fire if ready
+		ControlledTank->Fire(); // TODO limit firing rate
 	}
 }
-
-ATank* ATankAIController::GetControlledTank() const
-{
-	return Cast<ATank>(GetPawn());
-}
-
-ATank* ATankAIController::GetPlayerTank() const
-{
-	auto PlayerPawn = GetWorld()->GetFirstPlayerController()->GetPawn();
-	if (!PlayerPawn) { return nullptr; }
-	return Cast<ATank>(PlayerPawn);
-}
-
-
-
--- BattleTank/Source/BattleTank/TankAIController.h
+++ BattleTank/Source/BattleTank/TankAIController.h
@@ -16,15 +16,8 @@ UCLASS()
 class BATTLETANK_API ATankAIController : public AAIController
 {
 	GENERATED_BODY()
-	
-private:
-	ATank* GetControlledTank() const;
-
-	ATank* GetPlayerTank() const;
 
 public:
-	virtual void BeginPlay() override;
-
 	virtual void Tick(float DeltaTime) override;
 
 };
